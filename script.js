85ed3a39435f18fb5ac170d4fc3e4e86579d8619
ThiagoWebsites <143205226+ThiagoWebsites@users.noreply.github.com>
2024-04-08 16:58:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let Soma1 = document.querySelector ("#primeira")
+let Soma2 = document.querySelector ("#segunda")
+let resultado = document.querySelector ("#resultado")
+let botao = document.querySelector ("#botao")
+
+botao.addEventListener("click", recebervalores)
+
+
+
